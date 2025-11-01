5bc4589cec8e4e4f4a5713b0d1f348bc86e12b11
Kabir Yusuf Bashir <47000237+kabiryusufbashir@users.noreply.github.com>
2025-11-01 16:55:04 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <title>PHML Register Converter (PDF Version)</title>
+    </head>
+    <body class="min-h-screen my-64 bg-gray-100">
+        <div class="text-center text-3xl mb-6">
+            PHML Register Converter (PDF Version)
+        </div>
+        <div class="text-center">
+            Version 2.0
+        </div>
+        <div class="flex justify-center gap-4">
+            <a href="./scripts/summary.php">
+                <div class="m-4 bg-green-600 p-6 text-white rounded-lg shadow-lg">PDF Summary</div>
+            </a>
+            <a href="./scripts/hygeia.php">
+                <div class="m-4 bg-green-600 p-6 text-white rounded-lg shadow-lg">Hygeia</div>
+            </a>
+            <a href="./scripts/phml.php">
+                <div class="m-4 bg-green-600 p-6 text-white rounded-lg shadow-lg">PHML</div>
+            </a>
+        </div>
+    </body>
+</html>
